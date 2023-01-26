96d8242d3499ae19b0905aa179ace1a155019760
Jesse Giles <jesse.giles@telus.com>
2023-01-25 17:38:19 -0700

    updated dashboard controller with new local variables in show action, getting total product and category counts from the db to be displayed on admin dashboard, tested working, need to make it look nice

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,16 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['BASIC_AUTH_USERNAME'], password: ENV['BASIC_AUTH_PASSWORD']
   
   def show
+    @total_products = Product.count
+    @total_categories = Category.count
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(
+      :category_id,
+      :quantity
+    )
   end
 end
