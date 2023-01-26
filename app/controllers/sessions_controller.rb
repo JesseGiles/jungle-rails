85e0578802e2e12ffcce4cbc1be8090d6a33bcee
Jesse Giles <jesse.giles@telus.com>
2023-01-26 16:57:53 -0700

    update session and application controllers for create/destroy user sessions, set current user, update admin catagories controller as was missing basic_auth check, add login/signup links to top nav

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,19 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:email])
+    #if user exists AND password entered is correct
+    if user && user.authenticate(params[:password])
+      #save user id inside browser cookie
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      redirect_to '/login'
+    end
   end
 
   def destroy
+    session.delete(:user_id)
   end
 
 end
