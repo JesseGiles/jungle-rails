e2006d3169e3576503856842ab5902343df140a5
Jesse Giles <jesse.giles@telus.com>
2023-01-30 16:54:49 -0700

    update User model with class method authenticate_with_credentials, update sessions controller to use new authentication, complete tests for user_spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,16 @@ class User < ApplicationRecord
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 3 }
-  
+
+  before_save :email_cleanup
+
+  def email_cleanup
+    self.email = email.strip.downcase
+  end
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.strip.downcase)
+    user && user.authenticate(password)
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,20 +25,42 @@ RSpec.describe User, type: :model do
  
     it "does not save if email provided isnt unique (already exists in DB, not case sensitive)" do
       @newuser.save
-      @anotheruser = User.new(first_name: "Ray", last_name: "Stanz", email: "ghost@buster.COM", password: "anothertestpw", password_confirmation: "anothertestpw")
+      @anotheruser = User.new(first_name: "Ray", last_name: "Stanz", email: "GHOST@buster.com", password: "anothertestpw", password_confirmation: "anothertestpw")
       @anotheruser.save
       expect(@anotheruser).not_to be_valid
     end
 
-    it "saves when all fields were populated" do
-      @newuser.save
-      expect(@newuser).to be_valid
+    it "requires a first_name, last_name and email " do
+      @anotheruser = User.new(password: "testpw", password_confirmation: "testpw")
+      expect(@anotheruser).not_to be_valid
     end
 
     it "requires a password have a miniumum length of 3 characters" do
       @anotheruser = User.new(first_name: "Ray", last_name: "Stanz", email: "ghost@buster.COM", password: "pw", password_confirmation: "pw")
       expect(@anotheruser).not_to be_valid
     end
+  end
 
+  describe '.authenticate_with_credentials' do
+    before(:each) do
+      @newuser = User.new(first_name: "Pete", last_name: "Venkman", email: "ghost@buster.com", password: "testpw", password_confirmation: "testpw")
+    end
+
+    it 'should return an instance of the user if authenticated succesfully' do
+      @newuser.save
+      expect(User.authenticate_with_credentials("ghost@buster.com", "testpw")).to eq(@newuser)
+    end
+    it 'should return nil if authentication fails' do
+      @newuser.save
+      expect(User.authenticate_with_credentials("bust@ghoster.com", "testpw")).to eq(nil)
+    end
+    it 'should log in if spaces entered before/after email address' do
+      @newuser.save
+      expect(User.authenticate_with_credentials(" ghost@buster.com ", "testpw")).to eq (@newuser)
+    end
+    it 'should log in if wrong case is used for email address' do
+      @newuser.save
+      expect(User.authenticate_with_credentials("GHOst@busTER.com", "testpw")).to eq(@newuser)
+    end
   end
 end
